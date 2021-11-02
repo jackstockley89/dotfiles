c6bd0787afe88366664fbf909dcb264dd30226ed
Jack Stockley <jack.stockley@digital.justice.gov.uk>
2021-11-02 16:38:02 +0000

    Expand launch script
    expanding the launch script to include other environments test and
    development.

--- launch-laaops-env.bash
+++ launch-laaops-env.bash
@@ -13,6 +13,10 @@ ebs)  echo "${1}: ${ENV}"
         osascript .launch-env.scpt $ebsPRODDB $ebsPRODAPPONE $ebsPRODAPPTWO production
       elif [ ${ENV} == pre-prod ]; then
         osascript .launch-env.scpt $ebsPREPRODDB $ebsPREPRODAPPONE $ebsPREPRODAPPTWO staging
+      elif [ ${ENV} == test ]; then
+        osascript .launch-env.scpt $ebsPREPRODDB $ebsPREPRODAPPONE testing
+      elif [ ${ENV} == dev ]; then
+        osascript .launch-env.scpt $ebsPREPRODDB $ebsPREPRODAPPONE development
       else
         echo "Unknown Environment"
         exit
@@ -24,6 +28,10 @@ soa)  echo "${1}: ${ENV}"
         osascript .launch-env.scpt $soaPRODADMIN $soaPRODAPPONE $soaPRODAPPTWO $soaPRODAPPTHREE production
       elif [ ${ENV} == pre-prod ]; then
         osascript .launch-env.scpt $soaPREPRODADMIN $soaPREPRODAPPONE $soaPREPRODAPPTWO $soaPREPRODAPPTHREE staging
+      elif [ ${ENV} == test ]; then
+        osascript .launch-env.scpt $soaPREPRODADMIN $soaPREPRODAPPONE testing
+      elif [ ${ENV} == dev ]; then
+        osascript .launch-env.scpt $soaPREPRODADMIN $soaPREPRODAPPONE development
       else
         echo "Unknown Environment"
         exit
@@ -32,9 +40,13 @@ soa)  echo "${1}: ${ENV}"
 edrms) echo "${1}: ${ENV}"
        cd "${scriptHome}"
       if [ ${ENV} == prod ]; then
-        osascript .launch-env.scpt $edrmsPRODAPPONE $edrmsPRODAPPTWO
+        osascript .launch-env.scpt $edrmsPRODAPPONE $edrmsPRODAPPTWO production
       elif [ ${ENV} == pre-prod ]; then
-        osascript .launch-env.scpt $edrmsPREPRODAPPONE $edrmsPREPRODAPPTWO
+        osascript .launch-env.scpt $edrmsPREPRODAPPONE $edrmsPREPRODAPPTWO staging
+      elif [ ${ENV} == test ]; then
+        osascript .launch-env.scpt $edrmsPREPRODAPPONE $edrmsPREPRODAPPTWO testing
+      elif [ ${ENV} == dev ]; then
+        osascript .launch-env.scpt $edrmsPREPRODAPPONE $edrmsPREPRODAPPTWO development
       else
         echo "Unknown Environment"
         exit
