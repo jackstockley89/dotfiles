b67fa8bee7ab56125a0e781c0a2b0b5510edc51f
Jack Stockley <jack.stockley@digital.justice.gov.uk>
2021-12-14 10:41:18 +0000

    Adding data collection
    Adding a script that will pull down relavent data need to connect to
    instances from the bastion, this will allow for quicker connection the
    backend servers when troubleshooting issues and doing changes. Updated
    current script to work with the changes put in place with the new
    data collection script

---
+++ environment-collection.bash
@@ -0,0 +1,170 @@
+#!bin/bash
+set -x 
+
+properties=~/properties
+devfile=.development.txt
+testfile=.testing.txt
+preprodfile=.staging.txt
+prodfile=.production.txt
+
+cp -r properties ~
+cp $properties/laaopsproperties.txt $properties/.laaopsproperties
+
+#collect a list of environments and place them in a text file 
+## DEV
+aws-vault exec laa-development-lz -- aws ec2 describe-instances \
+--filter "Name=tag:Name,Values=ccms-*" \
+--query "Reservations [*].Instances[*].{Instance:PrivateIpAddress,AZ:Placement.AvailabilityZone,Name:Tags[?Key=='Name'].Value | [0]}"  \
+--output text > $properties/.development.txt
+
+## EBS
+output=`awk '/eu-west-2a/ && /ccms-ebs DB Server/ { print $2 }' $properties/$devfile`
+sed -i '' 's/ebs.dev.db=<INPUT>/ebs.dev.db='${output}'/g' $properties/.laaopsproperties
+output=`awk '/eu-west-2a/ && /ccms-ebs App Server/ { print $2 }' $properties/$devfile`
+sed -i '' 's/ebs.dev.appA=<INPUT>/ebs.dev.appA='${output}'/g' $properties/.laaopsproperties
+output=`awk '/eu-west-2b/ && /ccms-ebs App Server Two/ { print $2 }' $properties/$devfile`
+sed -i '' 's/ebs.dev.appB=<INPUT>/ebs.dev.appB='${output}'/g' $properties/.laaopsproperties
+output=`awk '/eu-west-2c/ && /ccms-ebs App Server Three/ { print $2 }' $properties/$devfile`
+sed -i '' 's/ebs.dev.appC=<INPUT>/ebs.dev.appC='${output}'/g' $properties/.laaopsproperties
+
+## SOA
+output=`awk '/ccms-soa-ecs-cluster-admin/ { print $2 }' $properties/$devfile`
+sed -i '' 's/soa.dev.admin=<INPUT>/soa.dev.admin='${output}'/g' $properties/.laaopsproperties
+output=`awk '/eu-west-2a/ && /ccms-soa-ecs-cluster-managed/ { print $2 }' $properties/$devfile`
+sed -i '' 's/soa.dev.appA=<INPUT>/soa.dev.appA='${output}'/g' $properties/.laaopsproperties
+output=`awk '/eu-west-2b/ && /ccms-soa-ecs-cluster-managed/ { print $2 }' $properties/$devfile`
+sed -i '' 's/soa.dev.appB=<INPUT>/soa.dev.appB='${output}'/g' $properties/.laaopsproperties
+output=`awk '/eu-west-2c/ && /ccms-soa-ecs-cluster-managed/ { print $2 }' $properties/$devfile`
+sed -i '' 's/soa.dev.appC=<INPUT>/soa.dev.appC='${output}'/g' $properties/.laaopsproperties
+
+## EDRMS
+output=`awk '/eu-west-2a/ && /ccms-edrms-ecs-cluster/ { print $2 }' $properties/$devfile`
+sed -i '' 's/edrms.dev.appA=<INPUT>/edrms.dev.appA='${output}'/g' $properties/.laaopsproperties
+output=`awk '/eu-west-2b/ && /ccms-edrms-ecs-cluster/ { print $2 }' $properties/$devfile`
+sed -i '' 's/edrms.dev.appB=<INPUT>/edrms.dev.appB='${output}'/g' $properties/.laaopsproperties
+output=`awk '/eu-west-2c/ && /ccms-edrms-ecs-cluster/ { print $2 }' $properties/$devfile`
+sed -i '' 's/edrms.dev.appC=<INPUT>/edrms.dev.appC='${output}'/g' $properties/.laaopsproperties
+
+## OPA
+
+
+## PUI
+
+## TEST
+aws-vault exec laa-test-lz -- aws ec2 describe-instances \
+--filter "Name=tag:Name,Values=ccms-*" \
+--query "Reservations [*].Instances[*].{Instance:PrivateIpAddress,AZ:Placement.AvailabilityZone,Name:Tags[?Key=='Name'].Value | [0]}"  \
+--output text > $properties/.testing.txt
+
+## EBS
+output=`awk '/eu-west-2a/ && /ccms-ebs DB Server/ { print $2 }' $properties/$testfile`
+sed -i '' 's/ebs.test.db=<INPUT>/ebs.test.db='${output}'/g' $properties/.laaopsproperties
+output=`awk '/eu-west-2a/ && /ccms-ebs App Server/ { print $2 }' $properties/$testfile`
+sed -i '' 's/ebs.test.appA=<INPUT>/ebs.test.appA='${output}'/g' $properties/.laaopsproperties
+output=`awk '/eu-west-2b/ && /ccms-ebs App Server Two/ { print $2 }' $properties/$testfile`
+sed -i '' 's/ebs.test.appB=<INPUT>/ebs.test.appB='${output}'/g' $properties/.laaopsproperties
+output=`awk '/eu-west-2c/ && /ccms-ebs App Server Three/ { print $2 }' $properties/$testfile`
+sed -i '' 's/ebs.test.appC=<INPUT>/ebs.test.appC='${output}'/g' $properties/.laaopsproperties
+
+## SOA
+output=`awk '/ccms-soa-ecs-cluster-admin/ { print $2 }' $properties/$testfile`
+sed -i '' 's/soa.test.admin=<INPUT>/soa.test.admin='${output}'/g' $properties/.laaopsproperties
+output=`awk '/eu-west-2a/ && /ccms-soa-ecs-cluster-managed/ { print $2 }' $properties/$testfile`
+sed -i '' 's/soa.test.appA=<INPUT>/soa.test.appA='${output}'/g' $properties/.laaopsproperties
+output=`awk '/eu-west-2b/ && /ccms-soa-ecs-cluster-managed/ { print $2 }' $properties/$testfile`
+sed -i '' 's/soa.test.appB=<INPUT>/soa.test.appB='${output}'/g' $properties/.laaopsproperties
+output=`awk '/eu-west-2c/ && /ccms-soa-ecs-cluster-managed/ { print $2 }' $properties/$testfile`
+sed -i '' 's/soa.test.appC=<INPUT>/soa.test.appC='${output}'/g' $properties/.laaopsproperties
+
+## EDRMS
+output=`awk '/eu-west-2a/ && /ccms-edrms-ecs-cluster/ { print $2 }' $properties/$testfile`
+sed -i '' 's/edrms.test.appA=<INPUT>/edrms.test.appA='${output}'/g' $properties/.laaopsproperties
+output=`awk '/eu-west-2b/ && /ccms-edrms-ecs-cluster/ { print $2 }' $properties/$testfile`
+sed -i '' 's/edrms.test.appB=<INPUT>/edrms.test.appB='${output}'/g' $properties/.laaopsproperties
+output=`awk '/eu-west-2c/ && /ccms-edrms-ecs-cluster/ { print $2 }' $properties/$testfile`
+sed -i '' 's/edrms.test.appC=<INPUT>/edrms.test.appC='${output}'/g' $properties/.laaopsproperties
+
+## OPA
+
+
+## PUI
+
+## PRE-PROD
+aws-vault exec laa-staging-lz -- aws ec2 describe-instances \
+--filter "Name=tag:Name,Values=ccms-*" \
+--query "Reservations [*].Instances[*].{Instance:PrivateIpAddress,AZ:Placement.AvailabilityZone,Name:Tags[?Key=='Name'].Value | [0]}"  \
+--output text > $properties/.staging.txt
+
+## EBS
+output=`awk '/eu-west-2a/ && /ccms-ebs DB Server/ { print $2 }' $properties/$preprodfile`
+sed -i '' 's/ebs.preprod.db=<INPUT>/ebs.preprod.db='${output}'/g' $properties/.laaopsproperties
+output=`awk '/eu-west-2a/ && /ccms-ebs App Server/ { print $2 }' $properties/$preprodfile`
+sed -i '' 's/ebs.preprod.appA=<INPUT>/ebs.preprod.appA='${output}'/g' $properties/.laaopsproperties
+output=`awk '/eu-west-2b/ && /ccms-ebs App Server Two/ { print $2 }' $properties/$preprodfile`
+sed -i '' 's/ebs.preprod.appB=<INPUT>/ebs.preprod.appB='${output}'/g' $properties/.laaopsproperties
+output=`awk '/eu-west-2c/ && /ccms-ebs App Server Three/ { print $2 }' $properties/$preprodfile`
+sed -i '' 's/ebs.preprod.appC=<INPUT>/ebs.preprod.appC='${output}'/g' $properties/.laaopsproperties
+
+## SOA
+output=`awk '/ccms-soa-ecs-cluster-admin/ { print $2 }' $properties/$preprodfile`
+sed -i '' 's/soa.preprod.admin=<INPUT>/soa.preprod.admin='${output}'/g' $properties/.laaopsproperties
+output=`awk '/eu-west-2a/ && /ccms-soa-ecs-cluster-managed/ { print $2 }' $properties/$preprodfile`
+sed -i '' 's/soa.preprod.appA=<INPUT>/soa.preprod.appA='${output}'/g' $properties/.laaopsproperties
+output=`awk '/eu-west-2b/ && /ccms-soa-ecs-cluster-managed/ { print $2 }' $properties/$preprodfile`
+sed -i '' 's/soa.preprod.appB=<INPUT>/soa.preprod.appB='${output}'/g' $properties/.laaopsproperties
+output=`awk '/eu-west-2c/ && /ccms-soa-ecs-cluster-managed/ { print $2 }' $properties/$preprodfile`
+sed -i '' 's/soa.preprod.appC=<INPUT>/soa.preprod.appC='${output}'/g' $properties/.laaopsproperties
+
+## EDRMS
+output=`awk '/eu-west-2a/ && /ccms-edrms-ecs-cluster/ { print $2 }' $properties/$preprodfile`
+sed -i '' 's/edrms.preprod.appA=<INPUT>/edrms.preprod.appA='${output}'/g' $properties/.laaopsproperties
+output=`awk '/eu-west-2b/ && /ccms-edrms-ecs-cluster/ { print $2 }' $properties/$preprodfile`
+sed -i '' 's/edrms.preprod.appB=<INPUT>/edrms.preprod.appB='${output}'/g' $properties/.laaopsproperties
+output=`awk '/eu-west-2c/ && /ccms-edrms-ecs-cluster/ { print $2 }' $properties/$preprodfile`
+sed -i '' 's/edrms.preprod.appC=<INPUT>/edrms.preprod.appC='${output}'/g' $properties/.laaopsproperties
+
+## OPA
+
+
+## PUI
+
+## PROD
+aws-vault exec laa-production-lz -- aws ec2 describe-instances \
+--filter "Name=tag:Name,Values=ccms-*" \
+--query "Reservations [*].Instances[*].{Instance:PrivateIpAddress,AZ:Placement.AvailabilityZone,Name:Tags[?Key=='Name'].Value | [0]}"  \
+--output text > $properties/.production.txt
+
+## EBS
+output=`awk '/eu-west-2a/ && /ccms-ebs DB Server/ { print $2 }' $properties/$prodfile`
+sed -i '' 's/ebs.prod.db=<INPUT>/ebs.prod.db='${output}'/g' $properties/.laaopsproperties
+output=`awk '/eu-west-2a/ && /ccms-ebs App Server/ { print $2 }' $properties/$prodfile`
+sed -i '' 's/ebs.prod.appA=<INPUT>/ebs.prod.appA='${output}'/g' $properties/.laaopsproperties
+output=`awk '/eu-west-2b/ && /ccms-ebs App Server Two/ { print $2 }' $properties/$prodfile`
+sed -i '' 's/ebs.prod.appB=<INPUT>/ebs.prod.appB='${output}'/g' $properties/.laaopsproperties
+output=`awk '/eu-west-2c/ && /ccms-ebs App Server Three/ { print $2 }' $properties/$prodfile`
+sed -i '' 's/ebs.prod.appC=<INPUT>/ebs.prod.appC='${output}'/g' $properties/.laaopsproperties
+
+## SOA
+output=`awk '/ccms-soa-ecs-cluster-admin/ { print $2 }' $properties/$prodfile`
+sed -i '' 's/soa.prod.admin=<INPUT>/soa.prod.admin='${output}'/g' $properties/.laaopsproperties
+output=`awk '/eu-west-2a/ && /ccms-soa-ecs-cluster-managed/ { print $2 }' $properties/$prodfile`
+sed -i '' 's/soa.prod.appA=<INPUT>/soa.prod.appA='${output}'/g' $properties/.laaopsproperties
+output=`awk '/eu-west-2b/ && /ccms-soa-ecs-cluster-managed/ { print $2 }' $properties/$prodfile`
+sed -i '' 's/soa.prod.appB=<INPUT>/soa.prod.appB='${output}'/g' $properties/.laaopsproperties
+output=`awk '/eu-west-2c/ && /ccms-soa-ecs-cluster-managed/ { print $2 }' $properties/$prodfile`
+sed -i '' 's/soa.prod.appC=<INPUT>/soa.prod.appC='${output}'/g' $properties/.laaopsproperties
+
+## EDRMS
+output=`awk '/eu-west-2a/ && /ccms-edrms-ecs-cluster/ { print $2 }' $properties/$prodfile`
+sed -i '' 's/edrms.prod.appA=<INPUT>/edrms.prod.appA='${output}'/g' $properties/.laaopsproperties
+output=`awk '/eu-west-2b/ && /ccms-edrms-ecs-cluster/ { print $2 }' $properties/$prodfile`
+sed -i '' 's/edrms.prod.appB=<INPUT>/edrms.prod.appB='${output}'/g' $properties/.laaopsproperties
+output=`awk '/eu-west-2c/ && /ccms-edrms-ecs-cluster/ { print $2 }' $properties/$prodfile`
+sed -i '' 's/edrms.prod.appC=<INPUT>/edrms.prod.appC='${output}'/g' $properties/.laaopsproperties
+
+## OPA
+
+
+## PUI
+
+sed -i '' 's/^.*=$//g' $properties/.laaopsproperties
--- launch-laaops-env.bash
+++ launch-laaops-env.bash
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 ENV=$2
-scriptHome=~
-. ~/properties/.laaopsproperties
+
+function prop {
+    grep "${1}" ~/properties/.laaopsproperties|cut -d'=' -f2
+}
 
 ## open env
 case "$1" in
@@ -10,48 +12,55 @@ case "$1" in
 ebs)  echo "${1}: ${ENV}"
       cd ${scriptHome}
       if [ ${ENV} == prod ]; then
-        osascript .launch-env.scpt $ebsPRODDB $ebsPRODAPPONE $ebsPRODAPPTWO production
+        osascript .launch-env-3.scpt $(prop 'ebs.prod.*') production
       elif [ ${ENV} == pre-prod ]; then
-        osascript .launch-env.scpt $ebsPREPRODDB $ebsPREPRODAPPONE $ebsPREPRODAPPTWO staging
+        osascript .launch-env-3.scpt $(prop 'ebs.preprod.*') staging
       elif [ ${ENV} == test ]; then
-        osascript .launch-env.scpt $ebsPREPRODDB $ebsPREPRODAPPONE testing
+        osascript .launch-env-2.scpt $(prop 'ebs.test.*') test
       elif [ ${ENV} == dev ]; then
-        osascript .launch-env.scpt $ebsPREPRODDB $ebsPREPRODAPPONE development
+        osascript .launch-env-2.scpt $(prop 'ebs.dev.*') development
       else
-        echo "Unknown Environment"
+        echo "Unknown Environment"    
         exit
       fi
       ;;
 soa)  echo "${1}: ${ENV}"
-      cd "${scriptHome}"
+      cd ${scriptHome}
       if [ ${ENV} == prod ]; then
-        osascript .launch-env.scpt $soaPRODADMIN $soaPRODAPPONE $soaPRODAPPTWO $soaPRODAPPTHREE production
+        osascript .launch-env-4.scpt $(prop 'soa.prod.*') production
       elif [ ${ENV} == pre-prod ]; then
-        osascript .launch-env.scpt $soaPREPRODADMIN $soaPREPRODAPPONE $soaPREPRODAPPTWO $soaPREPRODAPPTHREE staging
+        osascript .launch-env-4.scpt $(prop 'soa.preprod.*') staging
       elif [ ${ENV} == test ]; then
-        osascript .launch-env.scpt $soaPREPRODADMIN $soaPREPRODAPPONE testing
+        osascript .launch-env-2.scpt $(prop 'soa.test.*') test
       elif [ ${ENV} == dev ]; then
-        osascript .launch-env.scpt $soaPREPRODADMIN $soaPREPRODAPPONE development
+        osascript .launch-env-2.scpt $(prop 'soa.dev.*') development
       else
-        echo "Unknown Environment"
+        echo "Unknown Environment"    
         exit
       fi
       ;;
 edrms) echo "${1}: ${ENV}"
-       cd "${scriptHome}"
+      cd ${scriptHome}
       if [ ${ENV} == prod ]; then
-        osascript .launch-env.scpt $edrmsPRODAPPONE $edrmsPRODAPPTWO production
+        osascript .launch-env-2.scpt $(prop 'edrms.prod.*') production
       elif [ ${ENV} == pre-prod ]; then
-        osascript .launch-env.scpt $edrmsPREPRODAPPONE $edrmsPREPRODAPPTWO staging
+        osascript .launch-env-2.scpt $(prop 'edrms.preprod.*') staging
       elif [ ${ENV} == test ]; then
-        osascript .launch-env.scpt $edrmsPREPRODAPPONE $edrmsPREPRODAPPTWO testing
+        osascript .launch-env-2.scpt $(prop 'edrms.test.*') test
       elif [ ${ENV} == dev ]; then
-        osascript .launch-env.scpt $edrmsPREPRODAPPONE $edrmsPREPRODAPPTWO development
+        osascript .launch-env-2.scpt $(prop 'edrms.dev.*') development
       else
-        echo "Unknown Environment"
+        echo "Unknown Environment"    
         exit
       fi
       ;;
+help) echo "${1}"
+      echo "Application List: ebs, soa, edrms"
+      echo "Environment List: dev, test, pre-prod, prod"
+      echo "example: launch-env.scpt <Application> <Environement>"
+      ;;
 *)    echo "Unknown Enviroment ${ENV}"
+      echo "Use help to show script usage"
+      echo "launch-env.scpt help"
       ;;
 esac
