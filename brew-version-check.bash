b9b2caada7a7b10d0e7c689cc2d60830fac8c24d
Jack Stockley <jack.stockley@digital.justice.gov.uk>
2022-03-15 09:19:56 +0000

    feat: adding script to check package version
    script will check version of most used packages and will check for any
    outdated packages

---
+++ brew-version-check.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo -e "Current Package Versions"
+brew list --versions awscli git kubectl vim go tfenv aws-vault
+echo -e "Would you like to continue to view outdated package versions?"
+read -p '[y/n]: ' yesnovar
+
+if [ $yesnovar == n ]; then
+	echo -e "\nOutdated Versions"
+	outdated=`brew outdated`
+	echo $outdated
+elif [ $yesnovar == y ]; then
+	if [ -z $outdated ]; then
+		echo -e "\nNothing to upgrade"
+		exit 0
+	else
+		echo -e "\nWould you like to upgrade outdated packages?"
+		read -p '[y/n]: ' yesnovar
+		if [ $yesnovar == n ]; then
+			exit 0
+		elif [ $yesnovar == y ]; then
+			brew upgrade
+		else 
+			echo "Unknown Response $yesnovar"
+			exit 1
+		fi
+	fi
+else
+	echo "Unknown Response $yesnovar"
+	exit 1
+fi
