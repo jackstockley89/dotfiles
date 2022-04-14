531419e3bd35fffd75cae530ad9cd807846a53d0
Jack Stockley <jack.stockley@digital.justice.gov.uk>
2022-04-14 10:49:24 +0100

    feat: adding set tf workspace
    script will now update the workspace to match the namespace

--- kube/select-cluster-config.go
+++ kube/select-cluster-config.go
@@ -58,6 +58,10 @@ func listeksclusters() {
 // sets a new shell with the updated environment variables
 //
 func main() {
+	colourCyan := "\033[36m"
+	colourReset := "\033[0m"
+	colourYellow := "\033[33m"
+
 	var env string
 	var arg *string
 	var arg2 string
@@ -72,15 +76,13 @@ func main() {
 
 	for _, p := range profile {
 		if *arg == p {
-			fmt.Println("Profile:", p)
-
 			// set kubeconfig for live or test
 			os.Setenv("KUBECONFIG", home+"/.kube/"+p+"/config")
-			fmt.Println("KUBECONFIG:", os.Getenv("KUBECONFIG"))
+			fmt.Println(string(colourCyan), "KUBECONFIG:", string(colourReset), os.Getenv("KUBECONFIG"))
 
 			// set kube_config_path to kubeconfig
 			os.Setenv("KUBE_CONFIG_PATH", os.Getenv("KUBECONFIG"))
-			fmt.Println("KUBE_CONFIG_PATH:", os.Getenv("KUBE_CONFIG_PATH"))
+			fmt.Println(string(colourCyan), "KUBE_CONFIG_PATH:", string(colourReset), os.Getenv("KUBE_CONFIG_PATH"))
 
 			// set aws_profile to correct profile name
 			if p == "jacksapp" {
@@ -90,49 +92,61 @@ func main() {
 			} else {
 				os.Setenv("AWS_PROFILE", "moj-cp")
 			}
-			fmt.Println("AWS_PROFILE:", os.Getenv("AWS_PROFILE"))
+			fmt.Println(string(colourCyan), "AWS_PROFILE:", string(colourReset), os.Getenv("AWS_PROFILE"))
 
 			// set aws_config_file to correct path
 			os.Setenv("AWS_CONFIG_FILE", home+"/.aws/cloud-platform/config")
-			fmt.Println("AWS_CONFIG_FILE:", os.Getenv("AWS_CONFIG_FILE"))
+			fmt.Println(string(colourCyan), "AWS_CONFIG_FILE:", string(colourReset), os.Getenv("AWS_CONFIG_FILE"))
 
 			// set aws_shared_credentials_file to correct path
 			os.Setenv("AWS_SHARED_CREDENTIALS_FILE", home+"/.aws/cloud-platform/credentials")
-			fmt.Println("AWS_SHARED_CREDENTIALS_FILE:", os.Getenv("AWS_SHARED_CREDENTIALS_FILE"))
+			fmt.Println(string(colourCyan), "AWS_SHARED_CREDENTIALS_FILE:", string(colourReset), os.Getenv("AWS_SHARED_CREDENTIALS_FILE"))
 
 			// set aws_region to correct region
 			os.Setenv("AWS_REGION", "eu-west-2")
-			fmt.Println("AWS_REGION:", os.Getenv("AWS_REGION"))
+			fmt.Println(string(colourCyan), "AWS_REGION:", string(colourReset), os.Getenv("AWS_REGION"))
 
 			// set aws_default_region to correct region
 			os.Setenv("AWS_DEFAULT_REGION", os.Getenv("AWS_REGION"))
-			fmt.Println("AWS_DEFAULT_REGION:", os.Getenv("AWS_DEFAULT_REGION"))
+			fmt.Println(string(colourCyan), "AWS_DEFAULT_REGION:", string(colourReset), os.Getenv("AWS_DEFAULT_REGION"))
 
 			if *arg == "test" {
 				listeksclusters()
-				fmt.Println("Listing EKS Clusters")
+				fmt.Println(string(colourYellow), "Listing EKS Clusters", string(colourReset))
 				fmt.Println("Please select a cluster to use")
 				fmt.Scanln(&arg2)
 				cxtname = arg2
 			} else {
 				cxtname = *arg
 			}
-
-			fmt.Println("Context Name:", cxtname)
-
 			// update kube config to the new context if profile is test
 			if p == "test" {
-				fmt.Println("Updating Kube Config")
+				// set kubecontext to correct context name
+				fmt.Println(string(colourYellow), "Updating Kube Context")
 				cmd := exec.Command("aws", "eks", "update-kubeconfig", "--name", cxtname)
 				cmd.Run()
+
+				// tf workspace to the cluster name
+				fmt.Println(string(colourYellow), "Updating Terraform Workspace")
+				os.Setenv("TF_WORKSPACE", cxtname)
+				fmt.Println(string(colourCyan), "TF_WORKSPACE:", string(colourReset), os.Getenv("TF_WORKSPACE"))
+
+				// set command line prompt to comtext name
 				os.Setenv("PS1", "\\e[1;33m`kubectl config current-context`> \\e[m")
+				// start shell with new environment variables
 				syscall.Exec(os.Getenv("SHELL"), []string{os.Getenv("SHELL")}, os.Environ())
 				return
 			} else {
-				fmt.Println("Updating Kube Config")
 				// set kubecontext to correct context name
+				fmt.Println(string(colourYellow), "Updating Kube Context")
 				os.Setenv("KUBECONTEXT", cxtname+"."+os.Getenv("CP_ENV"))
-				fmt.Println("KUBECONTEXT:", os.Getenv("KUBECONTEXT"))
+				fmt.Println(string(colourCyan), "KUBECONTEXT:", string(colourReset), os.Getenv("KUBECONTEXT"))
+
+				// tf workspace to the cluster name
+				fmt.Println(string(colourYellow), "Updating Terraform Workspace")
+				os.Setenv("TF_WORKSPACE", cxtname)
+				fmt.Println(string(colourCyan), "TF_WORKSPACE:", string(colourReset), os.Getenv("TF_WORKSPACE"))
+
 				// set command line prompt to comtext name
 				os.Setenv("PS1", "\\e[1;33m`kubectl config current-context`> \\e[m")
 				// start shell with new environment variables
